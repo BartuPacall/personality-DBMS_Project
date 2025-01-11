cd47dc73c89919a6ba433f41426865c07a6b0923
BartuPacall <Bartup.123a@hotmail.com>
2025-01-11 22:49:19 +0300

    personality_data_updated_files

--- backend/server.js
+++ backend/server.js
@@ -1,41 +1,40 @@
-const {createPool} = require('mysql');
-const express = require('express');
-const cors = require('cors');
+const { createPool } = require("mysql2");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4000;
 
-
-app.use(cors())
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.json({message: "Hello World"});
+  res.json({ message: "Hello World" });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
-
 
 const db = createPool({
-    host: 'localhost',
-    password: 'db123',
-    user: 'root',
-    database: 'mbti_schema',
-    connectionLimit: 10
+  host: "localhost",
+  password: "B282R13t?",
+  user: "root",
+  database: "mbti_schema",
+  connectionLimit: 10,
 });
 
-
-app.get('/celebs', (req, res) => {
-    const name = req.query.name || req.body.name;  
-    db.query(`  select four_letter
+app.get("/celebs", (req, res) => {
+  const name = req.query.name || req.body.name;
+  db.query(
+    `  select four_letter
                 from celebs c
                 join letter l on l.celebs_id = c.id
-                where name= '${name}'`, (err, result) => {
-        if(err) {
-            console.log(err);
-        } else {
-            res.json(result);
-        }
-    });
+                where name= '${name}'`,
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json(result);
+      }
+    }
+  );
 });
-
--- frontend/dbms-personality-site/src/App.jsx
+++ frontend/dbms-personality-site/src/App.jsx
@@ -1,31 +1,28 @@
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 function App() {
- 
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const [data, setData] = useState([]);
   const [searchResult, setSearchResult] = useState(null);
 
-  
-
   const handleSearch = (event) => {
     event.preventDefault();
-    
 
     fetch(`http://127.0.0.1:4000/celebs?name=${name}`)
-      .then(response => response.json())
-      .then(data => {
-        setData(data)
-        setSearchResult(`Personality type of ${name} is, ${data.map((item) => item.four_letter)}`);
-        
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
+        setSearchResult(
+          `Personality type of ${name} is, ${data.map(
+            (item) => item.four_letter
+          )}`
+        );
       })
-      .catch(error => console.error(error));
- 
+      .catch((error) => console.error(error));
   };
 
   return (
-    <div >
+    <div>
       <h1>Find Personalities of Celebrities</h1>
       <form onSubmit={handleSearch}>
         <input
